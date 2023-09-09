822e4c62efa4f067ce651c85012919599ad95612
Jeffrey Chou <jc2573@cornell.edu>
2023-09-09 01:32:15 -0700

    Fix audio reference errors SSR

--- components/MusicCard.tsx
+++ components/MusicCard.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import Card from "./Card";
-import { useMemo, useState, useEffect } from "react";
+import { useMemo, useRef, useState, useEffect } from "react";
 import { wrap } from "popmotion";
 import { motion, AnimatePresence } from "framer-motion";
 
@@ -31,9 +31,23 @@ export default function MusicCard() {
 
     const { audioPath, imgPath, track, album, artist } = audioMap[wrappedAudioIndex]
 
-    const audio = useMemo(() => { return new Audio(audioPath); }, [audioIndex])
+    let leftRightAudio = useRef(typeof Audio !== "undefined" && new Audio());
+    let satelliteAudio = useRef(typeof Audio !== "undefined" && new Audio());
+    let ordinaryAudio = useRef(typeof Audio !== "undefined" && new Audio());
+    let dittoAudio = useRef(typeof Audio !== "undefined" && new Audio());
 
-    useEffect(() => { audio.play(); }, [audioIndex]);
+    const indexToRef = [leftRightAudio, satelliteAudio, ordinaryAudio, dittoAudio]
+
+    const audio = indexToRef[wrappedAudioIndex];
+
+    useEffect(() => {
+        leftRightAudio.current = new Audio('/left-right.mp3');
+        satelliteAudio.current = new Audio('/satellite.mp3');
+        ordinaryAudio.current = new Audio('/ordinary-life.mp3');
+        dittoAudio.current = new Audio('/ditto.mp3');
+    }, []);
+
+    useEffect(() => { audio.current.play(); }, [audioIndex]);
 
     return (<Card logoPath="/apple-music-logo.png" logoUrl="https://spotify.com" disableShadow={true}>
         <div className="relative p-[28px] h-full overflow-hidden rounded-2xl">
@@ -53,24 +67,24 @@ export default function MusicCard() {
                             <motion.svg animate={{ stroke: playing ? 'white' : '#00000090' }} className=' stroke-[1.6px] stroke-[#00000090]' width="22px" height="22px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M2 11h14M2 17h11M2 5h18" stroke-linecap="round" stroke-linejoin="round"></path><path d="M20 18.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zm0 0v-7.9a.6.6 0 01.6-.6H22" stroke-linecap="round"></path></motion.svg>
                         </a>
                     </button>
-                    <button onClick={() => { audio.pause(); setAudioIndex((prev) => { return prev - 1; }) }}>
+                    <button onClick={() => { audio.current.currentTime = 0; audio.current.pause(); setAudioIndex((prev) => { return prev - 1; }) }}>
                         <motion.svg animate={{ stroke: playing ? 'white' : '#00000090' }} className=' stroke-[1.6px] stroke-[#00000090]' width="22px" height="22px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M6 7v10M17.028 5.267a.6.6 0 01.972.471v12.524a.6.6 0 01-.972.47l-7.931-6.261a.6.6 0 010-.942l7.931-6.262z" stroke-linecap="round" stroke-linejoin="round"></path></motion.svg>                    </button>
-                    {playing ? <button onClick={() => { audio.pause(); setPlaying(false); }}>
+                    {playing ? <button onClick={() => { audio.current.pause(); setPlaying(false); }}>
                         <motion.svg layoutId="play" animate={{ stroke: playing ? 'white' : '#00000090' }} className=' stroke-[1.6px] stroke-[#00000090]' width="22px" height="22px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M6 18.4V5.6a.6.6 0 01.6-.6h2.8a.6.6 0 01.6.6v12.8a.6.6 0 01-.6.6H6.6a.6.6 0 01-.6-.6zM14 18.4V5.6a.6.6 0 01.6-.6h2.8a.6.6 0 01.6.6v12.8a.6.6 0 01-.6.6h-2.8a.6.6 0 01-.6-.6z"  ></path></motion.svg>
-                    </button> : <button onClick={() => { audio.play(); setPlaying(true); }}>
+                    </button> : <button onClick={() => { audio.current.play(); setPlaying(true); }}>
                         <motion.svg layoutId="play" animate={{ stroke: playing ? 'white' : '#00000090' }} className=' stroke-[1.6px] stroke-[#00000090]' width="22px" height="22px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M6.906 4.537A.6.6 0 006 5.053v13.894a.6.6 0 00.906.516l11.723-6.947a.6.6 0 000-1.032L6.906 4.537z" stroke-linecap="round" stroke-linejoin="round"></path></motion.svg>
                     </button>}
                     <button onClick={async () => {
-                        audio.pause(); await setAudioIndex((prev) => { return prev + 1; });
+                        audio.current.currentTime = 0; audio.current.pause(); await setAudioIndex((prev) => { return prev + 1; });
                     }}>
                         <motion.svg animate={{ stroke: playing ? 'white' : '#00000090' }} className=' stroke-[1.6px] stroke-[#00000090]' width="22px" height="22px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M18 7v10M6.972 5.267A.6.6 0 006 5.738v12.524a.6.6 0 00.972.47l7.931-6.261a.6.6 0 000-.942L6.972 5.267z" stroke-linecap="round" stroke-linejoin="round"></path></motion.svg>
                     </button>
                     {
                         muted ?
-                            <button onClick={() => { setMuted(false); audio.muted = false }}>
+                            <button onClick={() => { setMuted(false); audio.current.muted = false }}>
                                 <motion.svg animate={{ stroke: playing ? 'white' : '#00000090' }} className=' stroke-[1.6px] stroke-[#00000090]' width="22px" height="22px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><g clip-path="url(#sound-off_svg__clip0_3173_16686)"><path d="M18 14l2-2m2-2l-2 2m0 0l-2-2m2 2l2 2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M2 13.857v-3.714a2 2 0 012-2h2.9a1 1 0 00.55-.165l6-3.956a1 1 0 011.55.835v14.286a1 1 0 01-1.55.835l-6-3.956a1 1 0 00-.55-.165H4a2 2 0 01-2-2z"></path></g><defs><clipPath id="sound-off_svg__clip0_3173_16686"><path fill="#fff" d="M0 0h24v24H0z"></path></clipPath></defs></motion.svg>
                             </button> :
-                            <button onClick={() => { setMuted(true); audio.muted = true }}>
+                            <button onClick={() => { setMuted(true); audio.current.muted = true }}>
                                 <motion.svg animate={{ stroke: playing ? 'white' : '#00000090' }} className=' stroke-[1.6px] stroke-[#00000090]' width="22px" height="22px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M1 13.857v-3.714a2 2 0 012-2h2.9a1 1 0 00.55-.165l6-3.956a1 1 0 011.55.835v14.286a1 1 0 01-1.55.835l-6-3.956a1 1 0 00-.55-.165H3a2 2 0 01-2-2z" ></path><path d="M17.5 7.5S19 9 19 11.5s-1.5 4-1.5 4M20.5 4.5S23 7 23 11.5s-2.5 7-2.5 7" stroke-linecap="round" stroke-linejoin="round"></path></motion.svg>
                             </button>
                     }
