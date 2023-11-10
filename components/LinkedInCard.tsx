c7a0c5f5d1874de9784793c2d44735727ee1f078
Jeffrey Chou <jc2573@cornell.edu>
2023-11-09 22:44:46 -0800

    Add missing logo link for LinkedIn card

--- components/LinkedInCard.tsx
+++ components/LinkedInCard.tsx
@@ -10,9 +10,9 @@ export default function LinkedInCard() {
 
     return (<Card>
         <div className="relative w-full h-full overflow-hidden rounded-2xl p-2 bg-[#F3F2F0]">
-            <div className="z-[1000] absolute flex justify-center items-center right-[28px] top-[28px] h-[50px] w-[50px] rounded-lg overflow-hidden">
+            <a href="https://www.linkedin.com/in/jeffreychoucu/" className="z-[1000] absolute flex justify-center items-center right-[28px] top-[28px] h-[50px] w-[50px] rounded-lg overflow-hidden">
                 <svg xmlns="http://www.w3.org/2000/svg" height='30px' width='30px' viewBox="0 0 24 24"><path d="M6.94048 4.99993C6.94011 5.81424 6.44608 6.54702 5.69134 6.85273C4.9366 7.15845 4.07187 6.97605 3.5049 6.39155C2.93793 5.80704 2.78195 4.93715 3.1105 4.19207C3.43906 3.44699 4.18654 2.9755 5.00048 2.99993C6.08155 3.03238 6.94097 3.91837 6.94048 4.99993ZM7.00048 8.47993H3.00048V20.9999H7.00048V8.47993ZM13.3205 8.47993H9.34048V20.9999H13.2805V14.4299C13.2805 10.7699 18.0505 10.4299 18.0505 14.4299V20.9999H22.0005V13.0699C22.0005 6.89993 14.9405 7.12993 13.2805 10.1599L13.3205 8.47993Z" fill="rgba(255,255,255,1)"></path></svg>
-            </div>
+            </a>
             <motion.div animate={{ scale: hover ? 15 : 1, z: 1 }} transition={{ type: 'linear', duration: 0.2 }} className="z-10 absolute right-[28px] top-[28px] h-[50px] w-[50px] bg-gradient-to-b from-[#5d8ef9] to-[#0B65C2] rounded-lg">
             </motion.div>
             <div className={`relative h-full w-full border-light-gray border-2 p-4 rounded-xl ${hover ? "z-50  bg-opacity-0 border-opacity-30 text-white" : "bg-white"}`}>
